e755631c30482245eeb902316b9d6fd1b87ad858
PaablooCH <paabloo.ch@gmail.com>
2026-01-18 15:50:29 +0100

    feat: add ProjectsLayout and GoBack components
    Introduces a reusable ProjectsLayout component that wraps children with padding and a GoBack button. The GoBack component uses Next.js navigation to route users back to a specified URL, improving navigation within the projects section.

---
+++ src/components/goBack.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { useRouter } from "next/navigation";
+
+type GoBackProps = {
+    url: string;
+}
+
+export default function GoBack({ url }: GoBackProps) {
+    const router = useRouter();
+
+    function handleClick() {
+        router.push(url);
+    }
+
+    return (
+        <button onClick={handleClick} aria-label="goBack" className="cursor-pointer">
+            <ArrowLeftIcon className="size-6 stroke-2 text-white"/>
+        </button>
+    )
+}
